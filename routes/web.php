3a0a2fa7dde42da11afb82a682ca687c6fcda03e
David Avila <david.av.del.22@gmail.com>
2024-09-23 12:05:37 -0600

    Cambios de Prueba

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/create', function(){
+    return "Aquí se mostrará el formulario para crear";
+});
+
+Route::get('/posts/{post}', function ($post){
+    return "Aquí se mostrará el post {$post}";
+});
